ca2f0048409d7c3049015a3124be3fa1afddee7c
crypticplank <bplank03@gmail.com>
2022-05-31 14:36:00 -0400

    just pushing

--- advancedstorage_test.go
+++ advancedstorage_test.go
@@ -3,10 +3,7 @@ package advancedstorage
 import (
 	"bytes"
 	"encoding/gob"
-	"encoding/hex"
-	"fmt"
-	"io"
-	"log"
+	"reflect"
 	"testing"
 )
 
@@ -16,22 +13,27 @@ type TestStruct struct {
 	TestNumber int64
 }
 
+var (
+	Test = TestStruct{TestText: "testing", TestNumber: 420, TestData: []byte{0x41, 0x41}}
+)
+
 func TestStorage_WriteToFile(t *testing.T) {
 	var buffer bytes.Buffer
-	s, _ := New(&Options{Filename: "secrets", Verbose: true})
+	s, _ := New(&Options{Filename: "advancedstorage.test"})
 	encoder := gob.NewEncoder(&buffer)
-	encoder.Encode(TestStruct{TestText: "testing", TestNumber: 420, TestData: []byte{0x41, 0x41}})
+	encoder.Encode(Test)
 	s.WriteToFile(buffer.Bytes())
-	data, _ := io.ReadAll(s.Reader)
-	fmt.Println(hex.Dump(data))
+	// io.ReadAll(s.Reader)
 }
 
 func TestStorage_ReadFromFile(t *testing.T) {
-	s, _ := New(&Options{Filename: "secrets", Verbose: true})
+	s, _ := New(&Options{Filename: "advancedstorage.test"})
 	data, _ := s.ReadFromFile()
 	var test TestStruct
 	buffer := bytes.NewBuffer(data)
 	decoder := gob.NewDecoder(buffer)
 	decoder.Decode(&test)
-	log.Println(test)
+	if !reflect.DeepEqual(test, Test) {
+		t.Fatalf("Data could not be verified %s", t.Name())
+	}
 }
